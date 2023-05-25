9ba3bf1f9c9b1ab78d3314e12ede9c841443612d
ClementBaffourPalmer <clementpalmer84@gmail.com>
2023-05-25 03:31:33 +0000

    Refactor expense tracker code for better performance

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 import csv
+from terminaltables import AsciiTable
 
 project_name = "Ck Palmer Expense Tracker"
 underline = "================================="
 
+
 def display_menu():
     print("========== Welcome to Ck Expense Tracker ==========")
-    print("1. Add an expense")
+    print("1. Adding an expense")
     print("2. View expenses")
     print("3. Update an expense")
     print("4. Delete an expense")
     print("5. View expense summary")
     print("6. Exit Application")
-  
+
 
 def add_expense(file):
     # collect expense details from the user
     title = input("What should I call this expense? ==> ")
-    date = input("Enter date (DD-MM-YYYY): ")
-    description = input("Enter specification: ")
-    amount = input("Enter amount in Ghs: ")
+    amount = input("What is the amount (Ghs)? ==>")
+    description = input("Enter some description (optional) ==>")
 
     print("Title:\t", title)
-    print("Date:\t", date)
     print("Description:\t", description)
+    print("Adding Expenses")
 
+    data = [title, amount, description]
+    table_data = [["Title", "Amount", "Description"], data]
+    table = AsciiTable(table_data)
+    print(table.table)
     yes_or_no = input("Confirm you want to add this expense (yes/no): ")
 
     if yes_or_no.lower() in ["yes", "y"]:
         print("Persisting to database...")
+        # add to the csv file
         writer = csv.writer(file)
-        writer.writerow([title, date, description, amount])
+        writer.writerow(data)
+        print("Added to db")
         print("Expense recorded successfully!")
+        print(table.table)
+        print(underline)
     else:
         print("Aborted...")
 
-def view_expenses():
+
+def view_expenses(file):
     with open('expenses.csv', mode='r') as file:
         reader = csv.reader(file)
         expenses = list(reader)
@@ -42,13 +52,15 @@ def view_expenses():
     if len(expenses) == 0:
         print("No expenses found")
     else:
-        print("Expenses:")
-        for expense in expenses:
-            print(f"Title: {expense[0]}, Date: {expense[1]}, Description: {expense[2]}, Amount: GHS {expense[3]}")
+        print("Expense Details:")
+        table_data = [["Title", "Amount", "Description"]]
+        table_data.extend(expenses)
+        table = AsciiTable(table_data)
+        print(table.table)
 
-def update_expenses(expenses_file):
+
+def update_expenses(file):
     expense_id = input("Enter the expense ID to update: ")
-
     with open('expenses.csv', mode='r') as file:
         reader = csv.reader(file)
         expenses_list = list(reader)
@@ -62,7 +74,6 @@ def update_expenses(expenses_file):
             new_amount = input("Enter new amount in Ghc: ")
 
             yes_or_no = input("Confirm you want to update this expense (yes/no): ")
-
             if yes_or_no.lower() in ["yes", "y"]:
                 expenses_list[i] = [expense_id, new_date, new_description, new_amount]
                 print("Expense updated successfully!")
@@ -77,18 +88,18 @@ def update_expenses(expenses_file):
         writer = csv.writer(file)
         writer.writerows(expenses_list)
 
+
 def delete_expense():
     expense_id = input("Enter expense ID to delete: ")
-
     with open('expenses.csv', mode='r') as file:
         reader = csv.reader(file)
-        expense = list(reader)
+        expenses = list(reader)
 
     found = False
-    for i, expense in enumerate(expense):
+    for i, expense in enumerate(expenses):
         if expense[0] == expense_id:
             found = True
-            del expense[i]
+            del expenses[i]
             print("Expense deleted successfully!")
             break
 
@@ -97,7 +108,7 @@ def delete_expense():
 
     with open('expenses.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows(expense)
+        writer.writerows(expenses)
 
 def view_expense_summary():
     with open('expenses.csv', mode='r') as file:
@@ -108,17 +119,26 @@ def view_expense_summary():
         print("No expenses found")
     else:
         total_expenses = 0
-        print("Expense Summary:")
-        for expense in expenses:
-            title, date, description, amount = expense
-            amount = float(amount)
-            total_expenses += amount
-            print(f"Title: {title}, Amount: {amount}")
+        table_data =[["Title", "Amount"]]
 
-        print("Total Expenses:", total_expenses)
+        for expense in expenses:
+            title, *data = expense
+            amount = data[0]
+            try:
+                amount = float(amount)
+                total_expenses += amount
+                table_data.append([title, amount])
+            except ValueError:
+                table_data.append([title, "Invalid amount"])
+
+    table = AsciiTable(table_data)
+    print(table.table)
+    print("Total Expenses:", total_expenses)
+
 
 def exit_application():
     print("Exiting the application...")
+
 
 def main():
     print(underline)
@@ -126,30 +146,31 @@ def main():
     print(underline)
     display_menu()
     print(underline)
-    
+
     file = open("expenses.csv", "a", newline='')
 
-    range = -1
-    while range != 0:
-        range = int(input("What do you want to do: "))
-        print("Your option:", range)
-        
-        if range == 1:
+    choice = -1
+    while choice != 0:
+        choice = int(input("What do you want to do: "))
+        print("Your option:", choice)
+
+        if choice == 1:
             add_expense(file)
-        elif range == 2:
-            view_expenses()
-        elif range == 3:
+        elif choice == 2:
+            view_expenses(file)
+        elif choice == 3:
             update_expenses(file)
-        elif range == 4:
+        elif choice == 4:
             delete_expense()
-        elif range == 5:
+        elif choice == 5:
             view_expense_summary()
-        elif range == 6:
+        elif choice == 6:
             exit_application()
-        elif range == 0:
+        elif choice == 0:
             break
         else:
             print("Invalid choice. Please try again.")
+
 
 if __name__ == "__main__":
     main()
